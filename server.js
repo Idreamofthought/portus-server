09a9e0a78adf511199b10eb23aa6b0431138396f
Richard <rmjenkins42@hotmail.com>
2026-09-04 10:24:17 +0200

    Remove explicit PORT definition in server.js

--- server.js
+++ server.js
@@ -682,7 +682,6 @@ cleanupExpired();
 setInterval(cleanupExpired, 24 * 60 * 60 * 1000).unref();
 
 // Start server
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
