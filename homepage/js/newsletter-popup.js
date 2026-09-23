eb9f4ff9dc69ebafc074ed630c2a9a863470d6eb
Richard <rmjenkins42@hotmail.com>
2026-09-23 14:13:02 +0200

    Remove obsolete newsletter popup

--- homepage/js/newsletter-popup.js
+++
@@ -1,7 +0,0 @@
-// Opens the Buttondown confirmation popup on newsletter form submit.
-// Extracted from an inline onsubmit= handler so script-src can drop 'unsafe-inline'.
-document.querySelectorAll('form.newsletter-form[target="popupwindow"]').forEach((form) => {
-  form.addEventListener('submit', () => {
-    window.open('https://buttondown.com/richardjenkins09', 'popupwindow');
-  });
-});
